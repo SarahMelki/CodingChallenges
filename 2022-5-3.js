436c7c0864d1223baff576487384327e577ecc0c
Sarah Melki <sarahmelki.dev@gmail.com>
2022-05-10 17:25:32 +0300

    solved new challenge

---
+++ 2022-5-3.js
@@ -0,0 +1,15 @@
+// Remove String Spaces
+
+// Simple, remove the spaces from the string, then return the resultant string.
+
+
+function noSpace(x){
+    return x.split(' ').join('')
+}
+
+
+
+// function noSpace(x){
+//     let arr =  x.split(' ');
+//     return arr.reduce((a,b)=>a+b);
+//    }
