2c5dd9ea1636e2bc49ec2d14a55d6ba43a05a41b
Sarah Melki <sarahmelki.dev@gmail.com>
2022-05-18 11:16:39 +0300

    8kyu - Convert a String to a Number!

---
+++ 2022-5-18.js
@@ -0,0 +1,36 @@
+// 8kyu - Convert a String to a Number!
+
+// Note: This kata is inspired by Convert a Number to a String!. Try that one too.
+
+// Description
+// We need a function that can transform a string into a number. What ways of achieving this do you know?
+
+// Note: Don't worry, all inputs will be strings, and every string is a perfectly valid representation of an integral number.
+
+// Examples
+// "1234" --> 1234
+// "605"  --> 605
+// "1405" --> 1405
+// "-7" --> -7
+
+
+
+//P: string
+
+//R: number
+
+//E: // Examples
+// "1234" --> 1234
+// "605"  --> 605
+// "1405" --> 1405
+// "-7" --> -7
+
+//P: function taks string as parameter
+//transform string to number
+//return number
+
+
+
+const stringToNumber = function(str){
+    return Number(str)
+  }
