c6404dd1a892a9685007d9837eafc70148be1236
Sarah Melki <sarahmelki.dev@gmail.com>
2022-05-30 16:04:48 +0300

    add simple 8kyu challenge

---
+++ 8kyu/2022-05-29.js
@@ -0,0 +1,28 @@
+//8kyu - Sum of positive
+
+
+// You get an array of numbers, return the sum of all of the positives ones.
+
+// Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+// Note: if there is nothing to sum, the sum is default to 0.
+
+
+
+//array of numbers
+//return sum of positives from this array
+//[1,-4,7,12] => 1 + 7 + 12 = 20
+//filter positives
+//reduce to sum of positives
+
+function positiveSum(arr) {
+  let newArr = arr.filter((e) => e > 0)
+  return newArr.reduce((a,b) => a+b,0)
+}
+
+
+//refactored
+
+// function positiveSum(arr) {
+//     return arr.reduce((a,b)=> a + (b > 0 ? b : 0),0);
+//  }
