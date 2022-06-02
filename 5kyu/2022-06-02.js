2c500774e3e269ae0decdbc30e00a74f85686828
Sarah Melki <sarahmelki.dev@gmail.com>
2022-06-02 21:08:21 +0300

    Human Readable Time

---
+++ 5kyu/2022-06-02.js
@@ -0,0 +1,54 @@
+// 5kyu - Human Readable Time
+
+
+// Write a function, which takes a non-negative integer (seconds) as input and returns the time in a human-readable format (HH:MM:SS)
+
+// HH = hours, padded to 2 digits, range: 00 - 99
+// MM = minutes, padded to 2 digits, range: 00 - 59
+// SS = seconds, padded to 2 digits, range: 00 - 59
+// The maximum time never exceeds 359999 (99:59:59)
+
+// You can find some examples in the test fixtures.
+
+// Examples:
+// strictEqual(humanReadable(0), '00:00:00', 'humanReadable(0)');
+// strictEqual(humanReadable(59), '00:00:59', 'humanReadable(59)');
+// strictEqual(humanReadable(60), '00:01:00', 'humanReadable(60)');
+// strictEqual(humanReadable(90), '00:01:30', 'humanReadable(90)');
+// strictEqual(humanReadable(3599), '00:59:59', 'humanReadable(3599)');
+// strictEqual(humanReadable(3600), '01:00:00', 'humanReadable(3600)');
+// strictEqual(humanReadable(45296), '12:34:56', 'humanReadable(45296)');
+// strictEqual(humanReadable(86399), '23:59:59', 'humanReadable(86399)');
+// strictEqual(humanReadable(86400), '24:00:00', 'humanReadable(86400)');
+// strictEqual(humanReadable(359999), '99:59:59', 'humanReadable(359999)');
+
+
+
+
+
+function humanReadable(seconds) {
+    //finish sorting out the extreme cases
+    if (seconds < 0 || seconds > 359999) {
+      return null
+    }
+    if (seconds === 0) {
+      return '00:00:00'
+    }
+    // Do the math for the hours
+    let hours = Math.floor(seconds / 3600)
+    seconds = seconds - (hours * 3600)  //check how many seconds are remaining from original seconds value
+    if (hours < 10) {
+      hours = '0' + hours
+    }
+    // Do the math for the minutes
+    let minutes = Math.floor(seconds / 60)
+    seconds = seconds - (minutes * 60) //check how many seconds are remaining from original seconds value
+    if (minutes < 10) {
+      minutes = '0' + minutes
+    }
+    // Check if there are fewer than 10 seconds remaining to account for leading zero
+    if(seconds < 10) {
+      seconds = '0' + seconds
+    }
+    return `${hours}:${minutes}:${seconds}`
+  }
