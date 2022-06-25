796cf072e0e033418a2b6d1ffd1c27c32e6fabed
Sarah Melki <sarahmelki.dev@gmail.com>
2022-06-25 11:07:02 +0300

    shortest word

---
+++ 7kyu/2022-06-25.js
@@ -0,0 +1,22 @@
+// 7kyu - Shortest Word
+
+
+// Simple, given a string of words, return the length of the shortest word(s).
+
+// String will never be empty and you do not need to account for different data types.
+
+
+
+//E: assert.strictEqual(findShort("bitcoin take over the world maybe who knows perhaps"), 3);
+    // assert.strictEqual(findShort("turns out random test cases are easier than writing out basic ones"), 3); 
+    // assert.strictEqual(findShort("Let's travel abroad shall we"), 2);
+//R: return number --> length of shortest word in string
+
+
+
+function findShort(s){
+
+    let sArr = s.split(' ').sort((a, b) => a.length - b.length)
+    return sArr[0].length
+
+}
